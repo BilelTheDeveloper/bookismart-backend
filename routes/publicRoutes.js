66850a32653465c191b4a533a262126914cef839
almadorashop <almadora.shop@gmail.com>
2026-04-23 01:34:15 +0100

    Bookify Update

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getDiscoveryFeed, getWebsiteBySlug } from '../controllers/publicController';
+import { getDiscoveryFeed, getWebsiteBySlug } from '../controllers/publicController.js';
 
 // No protection middleware here -> These are 100% Public
 router.get('/discovery', getDiscoveryFeed);
