0cc85f2b870de7e007b1a74afb698128b37c21ef
almadorashop <almadora.shop@gmail.com>
2026-04-29 00:10:12 +0100

    Bookify Update

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import websiteRoutes from './routes/websiteroutes.js';
 import publicRoutes from './routes/publicRoutes.js';
-import bookingRoutes from './routes/bookingRoutes.js'; // Added Booking Routes
 
 /**
  * 1. DATABASE CONNECTION
@@ -109,7 +108,6 @@ app.use('/api/public', publicRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/merchant/website', websiteRoutes);
-app.use('/api/bookings', bookingRoutes); // Added Booking Route Entry
 
 // Health check endpoint (used by keep-alive pinger)
 app.get('/health', (req, res) => {
